a0df83d381d2e7910860f414a59ae86f7ee5e525
Arslanoglou Georgios <arslanoglou.georgios@gmail.com>
2025-03-05 10:37:53 +0200

    chore(libs): Update platform toolset for C++

--- gMediaTools/Forms/FrmMain.cs
+++ gMediaTools/Forms/FrmMain.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 using gMediaTools.Extensions;
 using gMediaTools.Models;
 using gMediaTools.Models.Encoder;
